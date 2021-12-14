a996bb8f8dce48034bd2c6699864c7cd12edacef
unknown <prashantstha1110@gmail.com>
2021-12-14 09:44:33 +0545

    Added source code of palindrome.dart

---
+++ palindrome.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main()
+{
+  
+  print("Give me a word : ");
+  String word=(stdin.readLineSync()!);      //reades line given by user to the string word
+  int length=word.length;
+  int check=0;
+  for(int i=0;i<length;i++)
+  {
+    if(word[i]==word[length-(i+1)])
+    {
+      check++;
+    }
+  }
+
+  if(check==length)
+    print("The given word " + word +" is a Palindrome.");
+
+  else 
+    print("The given word " + word +" is not a Palindrome.");
+    
+}
